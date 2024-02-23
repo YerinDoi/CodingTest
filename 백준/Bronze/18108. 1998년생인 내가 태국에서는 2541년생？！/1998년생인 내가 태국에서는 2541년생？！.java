69b80f5528eee6b774b44f6c0a9ae2a2c55918b1
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-02-23 09:35:45 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x,y;
+		y = sc.nextInt();
+		x = y - 543;
+		
+		System.out.println(x);
+	}
+
+}
