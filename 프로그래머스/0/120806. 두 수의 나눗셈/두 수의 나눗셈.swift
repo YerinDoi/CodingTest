5dcb00555e79b15d4858913574cea6b2d9ab2b3c
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-11-16 15:29:44 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+func solution(_ num1:Int, _ num2:Int) -> Int {
+    // var Res: Int = Int(Double(num1) / Double(num2) * 1000)
+    var Res : Int = 1000 * num1 / num2
+    return Res
+}
