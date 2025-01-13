d4140f2ecf823c32a4ed9aef891091e46531dcd9
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-01-13 16:38:36 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.13 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ sides:[Int]) -> Int {
+    let sortedSides = sides.sorted()
+    if sortedSides[2] < sortedSides[0] + sortedSides[1] {
+        return 1
+    } else {
+        return 2
+    }
+}
