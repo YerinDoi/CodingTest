b1833b1cd52be7813c7646375cd524c08439a615
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-01-13 15:42:31 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.04 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.swift
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 func solution(_ strlist:[String]) -> [Int] {
-    var countList:[Int] = []
-    for i in strlist {
-        countList.append(i.count)
+    var length:[Int] = []
+    for str in strlist {
+        length.append(str.count)
     }
-    return countList
+    return length
 }
