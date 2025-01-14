9ce98a152a97d20d2fb141701ab3298bf6247f4d
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-01-14 23:48:57 +0900

    [level 0] Title: 순서쌍의 개수, Time: 621.91 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.swift
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.swift
@@ -1,16 +1,6 @@
 import Foundation
 
 func solution(_ n: Int) -> Int {
-    var count = 0
-    
-    // 1부터 n까지 반복하여 a 값을 찾음
-    for a in 1...n {
-        // a가 n을 나누었을 때 나머지가 0이면 b는 자연수가 된다.
-        if n % a == 0 {
-            count += 1
-        }
-    }
-    
-    return count
+    return (1...n).filter { n % $0 == 0 }.count
 }
 
