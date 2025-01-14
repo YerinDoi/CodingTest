c1362a06dbfe484356b6a0ce77ae63c0eef10cd8
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-01-14 22:37:32 +0900

    [level 0] Title: 모음 제거, Time: 0.13 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.swift
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 func solution(_ my_string: String) -> String {
-    // 모음들을 Set으로 정의
-    let vowels: Set<Character> = ["a", "e", "i", "o", "u"]
-
-    // filter를 사용하여 모음이 아닌 문자만 남김
-    let result = my_string.filter { !vowels.contains($0) }
-    
-    // 결과를 문자열로 변환하여 반환
-    return String(result)
+    return my_string.filter { !["a", "e", "i", "o", "u"].contains($0)}
 }
