817863ccb2f1b779de2b8a027611e9c4299699a6
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-01-14 23:32:01 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
@@ -1,11 +1,15 @@
 import Foundation
 
+// func solution(_ my_string:String, _ n:Int) -> String {
+//     var result:String = ""
+//     for chr in my_string {
+//         for _ in 0..<n {
+//             result += String(chr)
+//         }
+//     }
+//     return result
+// }
+
 func solution(_ my_string:String, _ n:Int) -> String {
-    var result:String = ""
-    for chr in my_string {
-        for _ in 0..<n {
-            result += String(chr)
-        }
-    }
-    return result
+    return my_string.map { String(repeating: String($0), count: n) }.joined()
 }
