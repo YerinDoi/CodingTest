ca227df0274a4d2d188adbbb71cf0eac20613233
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-01-16 00:57:28 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.20 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.swift
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.swift
@@ -1,11 +1,13 @@
 import Foundation
 
 func solution(_ n:Int) -> [Int] {
-    var array:[Int] = []
-    for i in 1...n {
-        if i % 2 == 1 {
-            array.append(i)
-        }
-    }
-    return array.sorted()
+    // var array:[Int] = []
+    // for i in 1...n {
+    //     if i % 2 == 1 {
+    //         array.append(i)
+    //     }
+    // }
+    // return array.sorted()
+    
+    return (1...n).filter { $0 % 2 == 1 }
 }
