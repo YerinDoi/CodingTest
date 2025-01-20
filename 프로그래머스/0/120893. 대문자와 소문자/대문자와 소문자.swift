a908a989293163d9db0348f68f6d12e9e44a78ca
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-01-20 16:38:27 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.45 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.swift
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.swift
@@ -1,13 +1,17 @@
 import Foundation
 
+// func solution(_ my_string:String) -> String {
+//     return my_string.map { chr in 
+//         if chr.isUppercase {
+//             return chr.lowercased()
+//         } else if chr.isLowercase {
+//             return chr.uppercased()
+//         } else {
+//             return String(chr)
+//         }
+//     }.joined()
+// }
+
 func solution(_ my_string:String) -> String {
-    return my_string.map { chr in 
-        if chr.isUppercase {
-            return chr.lowercased()
-        } else if chr.isLowercase {
-            return chr.uppercased()
-        } else {
-            return String(chr)
-        }
-    }.joined()
+    return my_string.map { $0.isUppercase ? String($0).lowercased() : String($0).uppercased() }.joined()
 }
