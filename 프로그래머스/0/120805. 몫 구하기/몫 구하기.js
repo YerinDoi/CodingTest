fa8a6f6da95972ce85880d8c58e1a230f41e3c63
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-01-31 14:47:49 +0900

    [level 0] Title: 몫 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -1,4 +1,8 @@
 function solution(num1, num2) {
-    var answer = Math.floor(num1 / num2);
-    return answer;
+    return Math.floor(num1 / num2)
 }
+
+// Math.floor()가 필요한 이유
+// 1. 나눗셈 결과가 실수(소수점 포함)로 나오기 때문
+// 2. 정수 몫만 필요할 때 사용
+// 3. 양수든 음수든 항상 "내림"하여 정수로 변환
