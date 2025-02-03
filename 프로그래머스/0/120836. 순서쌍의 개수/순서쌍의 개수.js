251f312db9300603bb0be8d91c0c9e6d1d140405
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-02-03 16:07:23 +0900

    [level 0] Title: 순서쌍의 개수, Time: 6.58 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,6 +1,19 @@
+// function solution(n) {
+//     var answer = Array.from({ length: n }, (_, i) => i + 1)  // 1부터 n까지 배열 생성
+//                       .filter(a => n % a === 0)  // 약수만 필터링
+//                       .length;  // 약수의 개수 세기
+//     return answer;
+// }
+
+
+
 function solution(n) {
-    var answer = Array.from({ length: n }, (_, i) => i + 1)  // 1부터 n까지 배열 생성
-                      .filter(a => n % a === 0)  // 약수만 필터링
-                      .length;  // 약수의 개수 세기
-    return answer;
+    let count = 0;
+    
+    for (let i=1; i <= n; i++) {
+        if(n % i === 0) {
+            count++;
+        }
+    }
+    return count;
 }
