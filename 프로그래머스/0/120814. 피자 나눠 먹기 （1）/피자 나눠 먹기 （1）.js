251c4c71e5d964989c8f15806682c7834a4f1a12
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-02-03 22:09:36 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,3 +1,8 @@
+// function solution(n) {
+//     return n % 7 === 0 ? n / 7 : Math.trunc(n / 7) + 1;
+// }
+
+
 function solution(n) {
-    return n % 7 === 0 ? n / 7 : Math.trunc(n / 7) + 1;
+    return Math.ceil(n / 7);
 }
