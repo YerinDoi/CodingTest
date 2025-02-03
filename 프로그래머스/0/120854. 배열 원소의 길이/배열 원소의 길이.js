40c945c01536c415e69e8deff1d675e7b4813c8a
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-02-03 14:33:44 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.js
@@ -1,3 +1,12 @@
+// function solution(strlist) {
+//     return strlist.map(a => a.length);
+// }
+
 function solution(strlist) {
-    return strlist.map(a => a.length);
+    let answer = [];
+    
+    for (let i = 0; i < strlist.length; i++) {
+        answer.push(strlist[i].length);
+    }
+    return answer;
 }
