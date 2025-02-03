fe5201370447f785a17c57a42a20b2b194f7c201
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-02-03 15:45:02 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,3 +1,8 @@
+// function solution(my_string, letter) {
+//     return my_string.split('').filter(a => a !== letter).join('');
+// }
+
+// letter를 기준으로 쪼개져서 결국 letter 없어짐
 function solution(my_string, letter) {
-    return my_string.split('').filter(a => a !== letter).join('');
+    return my_string.split(letter).join('');
 }
