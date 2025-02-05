b8b65bbc2e877802e321f2bb18bb75c3f959f956
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-02-05 17:26:10 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,3 +1,5 @@
+// slice(start, end)는 배열의 특정 부분을 잘라서 새로운 배열을 반환
+// start부터 end - 1까지의 요소를 잘라서 반환 (end는 포함되지 않음)
 function solution(num_list, n) {
     let result = [];
     
@@ -8,5 +10,14 @@ function solution(num_list, n) {
     return result;
 }
 
-// slice(start, end)는 배열의 특정 부분을 잘라서 새로운 배열을 반환하는 메서드야.
-// start부터 end - 1까지의 요소를 잘라서 반환해. (end는 포함되지 않음)
+
+// splice(start, count): 배열에서 특정 개수의 요소를 잘라서 반환하고, 원본 배열에서 제거
+function solution(num_list, n) {
+    var answer = [];
+
+    while(num_list.length) {
+        answer.push(num_list.splice(0,n));
+    }
+
+    return answer;
+}
