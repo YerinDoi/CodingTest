53140fa44d81ada65b3cb548ee3c1341863e84e8
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-02-12 13:06:03 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,5 +1,10 @@
+// function solution(numbers) {
+//     var sortedN = numbers.sort((a, b) => a - b);  // 숫자 크기 순으로 정렬
+//     var n = numbers.length;
+//     return sortedN[n - 1] * sortedN[n - 2]; 
+// }
+
 function solution(numbers) {
-    var sortedN = numbers.sort((a, b) => a - b);  // 숫자 크기 순으로 정렬
-    var n = numbers.length;
-    return sortedN[n - 1] * sortedN[n - 2]; 
+    numbers.sort((a, b) => b - a);
+    return numbers[0]*numbers[1];
 }
