4af8d046264d5b7519cbd1dfbf698103f6c22db1
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-02-15 14:44:03 +0900

    [level 2] Title: 올바른 괄호, Time: 31.80 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,17 +1,14 @@
-function solution(s) {
+function solution(s){
     const stack = [];
     
-    for (let char of s) {
-        if (char === '(') {
-            stack.push(char);  // 여는 괄호는 스택에 넣기
+    for (const c of s) {
+        if (c === '(') {
+            stack.push(c);
         } else {
-            if (stack.length === 0) {
-                return false;  // 스택이 비어있는데 닫는 괄호가 나옴 -> 올바르지 않음
-            }
-            stack.pop();  // 여는 괄호 하나 제거
+            if (stack.length === 0) return false;
+            else stack.pop();
         }
     }
     
-    // 모든 문자를 처리한 후 스택이 비어있으면 올바른 괄호
     return stack.length === 0;
 }
