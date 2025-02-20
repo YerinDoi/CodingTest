d30c169763d68e6c37bd86fd0d6a658374b0030f
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-02-20 14:48:22 +0900

    [level 2] Title: H-Index, Time: 0.63 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,9 +1,9 @@
 function solution(citations) {
-    citations.sort((a, b) => b - a); 
+    citations.sort((a, b) => b - a);  // 내림차순 정렬
     for (let i = 0; i < citations.length; i++) {
         if (citations[i] < i + 1) {
-            return i;  
+            return i;  // i번 이상 인용된 논문이 i편 이상일 때, i값이 H-Index
         }
     }
-    return citations.length; 
+    return citations.length;  // 모든 논문이 해당 조건을 만족하면 논문 수가 H-Index
 }
