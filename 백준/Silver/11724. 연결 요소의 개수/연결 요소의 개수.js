9a6e4a591f0f0143c55608150fd120b5d03d8aec
yerin <81500522+YerinDoi@users.noreply.github.com>
2025-03-06 23:28:56 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 960 ms, Memory: 138592 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.js
@@ -0,0 +1,31 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const edges = input.slice(1).map((line) => line.split(' ').map(Number));
+
+const graph = Array.from({ length : n + 1 }, () => []);
+edges.forEach(([u, v]) => {
+    graph[u].push(v);
+    graph[v].push(u);
+})
+
+const visitied = Array(n + 1).fill(false);
+
+function dfs(node) {
+    visitied[node] = true;
+
+    for (const next of graph[node]) {
+        if (!visitied[next]) dfs(next);
+    }
+}
+
+let count = 0;
+
+for (let i = 1; i <= n; i++) {
+    if (!visitied[i]){
+        dfs(i);
+        count++;
+    }
+}
+
+console.log(count);
